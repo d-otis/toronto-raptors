678587bcc07937021cf5dbc13ee762ee71bc944f
Dan Foley <d-otis@users.noreply.github.com>
2024-01-26 18:32:38 -0600

    make API call re-usable

--- getRaptorsPlayerData.js
+++ getRaptorsPlayerData.js
@@ -1,47 +1,26 @@
-const axios = require("axios");
-const { saveFile } = require("./utils");
+const { saveFile, getPage, generateConfig } = require("./utils");
 
 const getPlayerData = async () => {
+  const playersUrl = "https://www.balldontlie.io/api/v1/players";
   let nextPage = 1;
   const allRaptorsPlayers = [];
 
-  // 1. hitting the API
-  // 2. pushing onto a results array
-  // 3. repeat (until end of pagination)
-
-  const getPage = async page => {
-    const config = {
-      url: "https://www.balldontlie.io/api/v1/players",
-      params: {
-        per_page: 100,
-        page: page,
-      },
-      method: "get",
-    };
-
-    try {
-      const response = await axios(config);
-      const { meta } = response.data;
-      const players = response.data.data;
-      const raptorPlayers = players.filter(player => player.team.id == 28);
-
-      nextPage = meta.next_page;
-
-      if (raptorPlayers.length) {
-        allRaptorsPlayers.push(...raptorPlayers);
-      }
-    } catch (error) {
-      console.error({ error });
-    }
-  };
-
   while (nextPage) {
+    console.log(`getting page: ${nextPage}`);
+
+    const { response, meta } = await getPage(
+      generateConfig(playersUrl, nextPage)
+    );
+    const players = response.data.data;
+    const raptorPlayers = players.filter(player => player.team.id == 28);
+
+    raptorPlayers.length && allRaptorsPlayers.push(...raptorPlayers);
+
     console.log(
-      `getting page: ${nextPage} |`,
       ` ${allRaptorsPlayers.length} players have been collected in the array`
     );
 
-    await getPage(nextPage);
+    nextPage = meta.next_page;
   }
 
   const playerIds = allRaptorsPlayers.map(player => player.id);
--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 const fs = require("node:fs");
+const axios = require("axios");
 
 const saveFile = async (data, key, filename) => {
   const jsonString = JSON.stringify({ data: { [key]: data } });
@@ -11,4 +12,26 @@ const saveFile = async (data, key, filename) => {
   }
 };
 
-module.exports = { saveFile };
+const getPage = async config => {
+  try {
+    const response = await axios(config);
+    const { meta } = response.data;
+
+    return { response, meta };
+  } catch (error) {
+    console.error({ error });
+  }
+};
+
+const generateConfig = (url, page) => {
+  return {
+    url: url,
+    params: {
+      per_page: 100,
+      page: page,
+    },
+    method: "get",
+  };
+};
+
+module.exports = { saveFile, getPage, generateConfig };
